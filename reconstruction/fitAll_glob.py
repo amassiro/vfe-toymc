388edd00a179bc7e21bbf933b52154c3549fa746
Suzannah Fraker <suzannah.fraker@gmail.com>
2016-07-15 22:12:46 -0400

    Simplify fitAll_glob.py

--- reconstruction/fitAll_glob.py
+++ reconstruction/fitAll_glob.py
@@ -1,57 +1,27 @@
+import glob
 import os
 import sys
 
-def fit_waveform(input_files, waveform, already_fit_files, dryrun=0):
-  for f in sorted(input_files):
-      if waveform not in f:
-        continue
-      if not f.startswith('mysample'):
-        continue
-      multifit(f, already_fit_files, dryrun)
-
-def multifit(f, already_fit_files, dryrun=0):
-  i = 'input/%s' % f
-  o = 'outputfit/%s' % f.replace('mysample', 'output')
-  logfile = f.replace('mysample', 'Example07.multifit.debugPulseChiSqSNNLS.moreoutput').replace('root', 'txt')
-  if os.path.exists(o):
-    #print "skipping %s --> %s because %s already exists." %(i, o, o)
-    already_fit_files += 1
-    return  # not recreating+overwriting existing files
-  print f
-  params = f.split('_')
-  NSAMPLES, NFREQ = params[3 : 5]
-  waveform = params[-1].split('.')[0]
-  toExec = 'bin/Example07.multifit %s %s %s %s %s' % (i, o, NSAMPLES, NFREQ, waveform)
-  toExec = 'bin/Example07.multifit.debugPulseChiSqSNNLS.moreoutput %s %s %s %s %s > %s &2>&1' % (i, o, NSAMPLES, NFREQ, waveform, logfile)
-  toExec = 'bin/Example07.multifit.chiSq.iterations %s %s %s %s %s' % (i, o, NSAMPLES, NFREQ, waveform)
-  print toExec
-  if (dryrun == 0) :
-      os.system(toExec)
-
+def main(files, overwrite=True, dryrun=0):
+    for f in files:
+        i = f
+        o = f.replace('input', 'outputfit').replace('mysample', 'output')
+        if not overwrite and os.path.exists(o):
+          #print "skipping %s --> %s because %s already exists." %(i, o, o)
+          continue  # not recreating+overwriting existing files
+        params = f.split('_')
+        NSAMPLES, NFREQ = params[4 : 6]
+        toExec = 'bin/Example07.multifit %s %s %s %s' % (i, o, NSAMPLES, NFREQ)
+        print toExec
+        if (dryrun == 0) :
+            os.system(toExec)
 
 if __name__ == '__main__':
 
-    print sys.argv
-
-    dryrun = 0
-    if len(sys.argv) > 1 :
-      print " dry run option:",
-      dryrun = sys.argv[1]
-      print dryrun
-
-    old_input_files = set()
-    input_files = set()
-    already_fit_files = 0
-    wfs = ["CRRC10", "CRRC20", "CRRC30", "CRRC43", "CRRC60", "CRRC90",
-           "QIE25", "QIE12", "QIE6"]
-    while True:
-      old_input_files = input_files
-      input_files = set(os.listdir('input'))
-      print len(old_input_files)
-      print len(input_files)
-      if input_files == old_input_files:
-          print "no new input files"
-          break
-      for wf in wfs:
-         fit_waveform(input_files, wf, already_fit_files)
-    print already_fit_files, "already fit files"
+    args = []
+    # for loop that converts arguments with a '*' to the corresponding files
+    for arg in sys.argv[1:]:
+        args += glob.glob(arg)
+    if not args:
+        args = glob.glob('input/mysample_*')
+    main(args)
